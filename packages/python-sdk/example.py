d5336602a376d60c7a0a5d53b2221534cd5a8d6c
James Murdza <james@jamesmurdza.com>
2025-03-07 12:23:41 +0000

    Don't print the password in the Python example

--- packages/python-sdk/example.py
+++ packages/python-sdk/example.py
@@ -13,7 +13,6 @@ print("Screen size:", desktop.get_screen_size())
 desktop.vnc_server.start()
 
 print("VNC URL:", desktop.vnc_server.get_url())
-print("VNC Password:", desktop.vnc_server.password)
 
 input("Press enter to continue...")
 
